3c2be6d4793c5feb22309400318a26fa81313fdf
2nist <canyonuke@gmail.com>
2026-01-19 00:45:01 -0500

    Fix base path for GitHub Pages deployment

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/gigmaster/' : '/',
+  base: '/gigmaster/',
   server: {
     port: 5173
   }
